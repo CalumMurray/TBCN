a4aeaba4832df7bf604f7cbc3f7119fa59c3b18d
Calum Murray <Cal_92@hotmail.co.uk>
2013-03-18 19:58:30 +0000

    Template for Database Connection.  Deleted erroneous files

---
+++ TBCN/DatabaseConnection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace TBCN
+{
+    //
+    class DatabaseConnection
+    {
+        private const string connStr = "SERVER=arlia.computing.dundee.ac.uk;USER=12ac3u03;DATABASE=12ac3d03;PORT=3306;PASSWORD=ab123c;";
+
+        public void connect()
+        {
+            
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                // Perform database operations
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            conn.Close();
+            Console.WriteLine("Done.");
+        }
+
+    }
+}
