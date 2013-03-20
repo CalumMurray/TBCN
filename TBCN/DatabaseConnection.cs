865572fea144c28b559706cbc5ce34aa60e10470
CalumMurray <Cal_92@hotmail.co.uk>
2013-03-20 22:42:16 +0000

    Template for Database connection

--- TBCN/DatabaseConnection.cs
+++ TBCN/DatabaseConnection.cs
@@ -5,31 +5,231 @@ using System.Text;
 
 using MySql.Data;
 using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace TBCN
 {
-    //
+    
     class DatabaseConnection
     {
         private const string connStr = "SERVER=arlia.computing.dundee.ac.uk;USER=12ac3u03;DATABASE=12ac3d03;PORT=3306;PASSWORD=ab123c;";
+        private MySqlConnection connection;
 
-        public void connect()
+        public DatabaseConnection()
         {
-            
-            MySqlConnection conn = new MySqlConnection(connStr);
+            connection = new MySqlConnection(connStr);
+        }
+
+        public void OpenConnection()
+        {
             try
             {
                 Console.WriteLine("Connecting to MySQL...");
-                conn.Open();
-                // Perform database operations
+                connection.Open();
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
-            conn.Close();
-            Console.WriteLine("Done.");
+            
         }
 
+        public void CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                Console.WriteLine("MySQL connection closed.");
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            
+        }
+
+        //TODO: Entity Framework and Linq?
+
+        //TODO: Prepared Statements!
+        //TODO: Stored Procedures!
+        //TODO: Locks/Logs/Priveleges/Views
+
+        //ExecuteNonQuery(): Used to execute a command that will not return any data, for example Insert, update or delete.
+        //ExecuteReader(): Returns 0 or more results e.g. SELECT
+
+
+        //INSERT statement
+        public void Insert(String sqlInsertString)
+        {
+
+            //string query = "INSERT INTO tableinfo (name, age) VALUES('John Smith', '33')";
+
+            try
+            {
+                //open connection
+                OpenConnection();
+
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(sqlInsertString, connection);
+
+                //Execute command
+                Console.WriteLine("Executing Insert: [ " + sqlInsertString + "].");
+                cmd.ExecuteNonQuery();
+                
+                //close connection
+                this.CloseConnection();
+            }
+            catch (MySqlException msqle)
+            {
+                Console.WriteLine(msqle.ToString());
+            }
+        }
+
+        //UPDATE statement
+        public void Update(String sqlUpdateString)
+        {
+            try
+            {
+                //open connection
+                OpenConnection();
+
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(sqlUpdateString, connection);
+
+                //Execute command
+                Console.WriteLine("Executing Update: [ " + sqlUpdateString + "].");
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+            catch (MySqlException msqle)
+            {
+                Console.WriteLine(msqle.ToString());
+            }
+        }
+
+        //DELETE statement
+        public void Delete(String sqlDeleteString)
+        {
+            try
+            {
+                //open connection
+                OpenConnection();
+
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(sqlDeleteString, connection);
+
+                //Execute command
+                Console.WriteLine("Executing Delete: [ " + sqlDeleteString + "].");
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+            catch (MySqlException msqle)
+            {
+                Console.WriteLine(msqle.ToString());
+            }
+        }
+
+        //SELECT statement
+        public List<string>[] Select(String sqlSelectString)
+        {
+
+            //Create a list to store the result
+            List< string >[] list = new List< string >[3];
+            list[0] = new List< string >();
+            list[1] = new List< string >();
+            list[2] = new List< string >();
+
+            try
+            {
+                OpenConnection();
+
+                //Prepared Statement
+                //MySqlCommand cmd = new MySqlCommand("SELECT * FROM admin WHERE admin_username=@val1 AND admin_password=PASSWORD(@val2)", connection);
+                //cmd.Parameters.AddWithValue("@val1", tboxUserName.Text);
+                //cmd.Parameters.AddWithValue("@val2", tboxPassword.Text);
+                //cmd.Prepare();
+
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(sqlSelectString, connection);
+                
+                //Create a data reader and Execute the command
+                Console.WriteLine("Executing Select: [ " + sqlSelectString + "].");
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                MySqlDataAdapter mysqlAdapater = new MySqlDataAdapter();
+                DataSet ds = new DataSet();
+                
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    for (int i = 0; i < list.Length; i++)
+                    {
+                        list[i].Add(dataReader[dataReader.GetName(i)].ToString());
+                        //dataReader.GetString(columnNumber);
+                        //list[0].Add(dataReader["id"].ToString());
+                        //list[1].Add(dataReader["name"].ToString());
+                        //list[2].Add(dataReader["age"].ToString());
+                    }
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close Connection
+                CloseConnection();
+
+                //return list to be displayed
+                return list;
+            }
+            catch (MySqlException msqle)
+            {
+                Console.WriteLine(msqle.ToString());
+            }
+
+            return null;
+        }
+
+        //count statement
+        public int Count(String query)
+        {
+            
+            int count = -1;
+
+            //Open Connection
+            try
+            {
+                OpenConnection();
+
+                //Create Mysql Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //ExecuteScalar will return one value
+                count = int.Parse(cmd.ExecuteScalar().ToString());
+
+                //close Connection
+                CloseConnection();
+
+                return count;
+            }
+            catch (MySqlException msqle)
+            {
+                Console.WriteLine(msqle.ToString());
+            }
+            return -1;
+        }
+
+        //Backup
+        public void Backup()
+        {
+        }
+
+        //Restore
+        public void Restore()
+        {
+        }
     }
 }
--- TBCN/frmMainSearch.Designer.cs
+++ TBCN/frmMainSearch.Designer.cs
@@ -28,33 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Search = new System.Windows.Forms.Button();
-            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnChildSearch = new System.Windows.Forms.Button();
+            this.txtChildSearch = new System.Windows.Forms.TextBox();
+            this.btnParentSearch = new System.Windows.Forms.Button();
+            this.txtParentSearch = new System.Windows.Forms.TextBox();
+            this.btnSearchEmpolyee = new System.Windows.Forms.Button();
+            this.txtSearchEmployee = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // Search
+            // btnChildSearch
             // 
-            this.Search.Location = new System.Drawing.Point(370, 202);
-            this.Search.Name = "Search";
-            this.Search.Size = new System.Drawing.Size(75, 23);
-            this.Search.TabIndex = 0;
-            this.Search.Text = "btnSearch";
-            this.Search.UseVisualStyleBackColor = true;
+            this.btnChildSearch.Location = new System.Drawing.Point(241, 21);
+            this.btnChildSearch.Name = "btnChildSearch";
+            this.btnChildSearch.Size = new System.Drawing.Size(106, 23);
+            this.btnChildSearch.TabIndex = 0;
+            this.btnChildSearch.Text = "Search Children";
+            this.btnChildSearch.UseVisualStyleBackColor = true;
             // 
-            // txtSearch
+            // txtChildSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(138, 204);
-            this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(222, 20);
-            this.txtSearch.TabIndex = 1;
+            this.txtChildSearch.Location = new System.Drawing.Point(13, 24);
+            this.txtChildSearch.Name = "txtChildSearch";
+            this.txtChildSearch.Size = new System.Drawing.Size(222, 20);
+            this.txtChildSearch.TabIndex = 1;
+            // 
+            // btnParentSearch
+            // 
+            this.btnParentSearch.Location = new System.Drawing.Point(241, 50);
+            this.btnParentSearch.Name = "btnParentSearch";
+            this.btnParentSearch.Size = new System.Drawing.Size(106, 23);
+            this.btnParentSearch.TabIndex = 0;
+            this.btnParentSearch.Text = "Search Parents";
+            this.btnParentSearch.UseVisualStyleBackColor = true;
+            // 
+            // txtParentSearch
+            // 
+            this.txtParentSearch.Location = new System.Drawing.Point(13, 53);
+            this.txtParentSearch.Name = "txtParentSearch";
+            this.txtParentSearch.Size = new System.Drawing.Size(222, 20);
+            this.txtParentSearch.TabIndex = 1;
+            // 
+            // btnSearchEmpolyee
+            // 
+            this.btnSearchEmpolyee.Location = new System.Drawing.Point(241, 79);
+            this.btnSearchEmpolyee.Name = "btnSearchEmpolyee";
+            this.btnSearchEmpolyee.Size = new System.Drawing.Size(106, 23);
+            this.btnSearchEmpolyee.TabIndex = 0;
+            this.btnSearchEmpolyee.Text = "Search Employees";
+            this.btnSearchEmpolyee.UseVisualStyleBackColor = true;
+            // 
+            // txtSearchEmployee
+            // 
+            this.txtSearchEmployee.Location = new System.Drawing.Point(13, 82);
+            this.txtSearchEmployee.Name = "txtSearchEmployee";
+            this.txtSearchEmployee.Size = new System.Drawing.Size(222, 20);
+            this.txtSearchEmployee.TabIndex = 1;
             // 
             // frmMainSearch
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(594, 513);
-            this.Controls.Add(this.txtSearch);
-            this.Controls.Add(this.Search);
+            this.ClientSize = new System.Drawing.Size(404, 192);
+            this.Controls.Add(this.txtSearchEmployee);
+            this.Controls.Add(this.txtParentSearch);
+            this.Controls.Add(this.txtChildSearch);
+            this.Controls.Add(this.btnSearchEmpolyee);
+            this.Controls.Add(this.btnParentSearch);
+            this.Controls.Add(this.btnChildSearch);
             this.Name = "frmMainSearch";
             this.Text = "frmMainSearch";
             this.ResumeLayout(false);
@@ -64,7 +104,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button Search;
-        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnChildSearch;
+        private System.Windows.Forms.TextBox txtChildSearch;
+        private System.Windows.Forms.Button btnParentSearch;
+        private System.Windows.Forms.TextBox txtParentSearch;
+        private System.Windows.Forms.Button btnSearchEmpolyee;
+        private System.Windows.Forms.TextBox txtSearchEmployee;
     }
 }
